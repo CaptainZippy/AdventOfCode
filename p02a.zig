2996dcd174bf46be27479a3a7dfeb5332b2d29a8
CaptainZippy <muchomojo@gmail.com>
2020-12-06 12:56:02 +0000

    Day 2

---
+++ p02a.zig
@@ -0,0 +1,34 @@
+
+const std = @import("std");
+
+
+pub fn main() !void {
+    var stdout = std.io.getStdOut().writer();
+    var memory: [2 * 1024 * 1024]u8 = undefined;
+    var fixedBuffer = std.heap.FixedBufferAllocator.init(&memory);
+    var alloc = &fixedBuffer.allocator;
+
+    const cwd = std.fs.cwd();
+    const input = try cwd.openFile("input02.txt", .{.read=true});
+    const size = (try input.stat()).size;
+    const bytes = try alloc.alloc(u8, size);
+    const res = input.read(bytes);
+    var it = std.mem.split(bytes, "\n");
+    var valid:i32 = 0;
+    while(it.next()) |line|{
+        if(line.len==0) continue;
+        var x = std.mem.tokenize(line, "-: ");
+        const lo_ = x.next() orelse return error.Broken;
+        const hi_ = x.next() orelse return error.Broken;
+        const chr = x.next() orelse return error.Broken;
+        const pass = x.next() orelse return error.Broken;
+        const lo = try std.fmt.parseInt(u32, lo_, 10);
+        const hi = try std.fmt.parseInt(u32, hi_, 10);
+        var n = std.mem.count(u8, pass, chr);
+        if (n >= lo and n <= hi) {
+            valid += 1;
+        }
+    }
+    try stdout.print("line {}\n", .{valid});
+
+}
